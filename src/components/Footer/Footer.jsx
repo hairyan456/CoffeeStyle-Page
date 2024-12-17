08ab484e829a3f5509a32afa202b1f4c67ae64a0
hairyan456 <hairyan789@gmail.com>
2024-12-17 14:17:25 +0700

    add subscribe us section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='h-[350px] bg-slate-400'>
             Footer
         </div>
     );
--- src/components/Main/Subscribe.jsx
+++ src/components/Main/Subscribe.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Subscribe = () => {
     return (
-        <div className='h-[500px] bg-slate-400'>
-
+        <div className='h-[350px] bg-[#1d1f2e] mb-24 flex flex-col justify-center items-center'>
+            <div className='flex justify-center items-center px-6 xs:px-0 mb-6'>
+                <div className='w-8 h-px bg-gray-600' />
+                <div className='uppercase mx-4 tracking-widest text-gray-400 font-medium text-xs text-center'>
+                    Sign up and get free coffee bags
+                </div>
+                <div className='w-8 h-px bg-gray-600' />
+            </div>
+            <div className='text-2xl mb-6 text-white uppercase'>Coffee Updates</div>
+            <div className='flex flex-col md:flex-row justify-center items-center gap-4'>
+                <div className='text-center'>
+                    <input type="email" placeholder='example@gmail.com'
+                        className='text-sm px-6 py-4 w-[350px] max-w-[80%] xs:max-w-full bg-inherit border-[1px] border-gray-700 outline-none text-white leading-5
+                        hover:border-gray-300 transition-all duration-500 ease-in-out focus:border-gray-300 placeholder:uppercase
+                        placeholder:text-xs placeholder:tracking-widest placeholder:font-medium' />
+                </div>
+                <div className=''>
+                    <button type='submit' className='ct-button bg-white text-gray-900 w-max max-h-[54px]'>Subcribe</button>
+                </div>
+            </div>
         </div>
     );
 };
